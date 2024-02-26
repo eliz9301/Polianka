7fc7cedbf754049a2f08d3cdadbb712ee6454126
bvzgjkz <122781449+bvzgjkz@users.noreply.github.com>
2024-02-27 05:59:08 +0600

    Задание готово к проверке

--- ConsoleApplication21/ConsoleApplication21.cpp
+++ ConsoleApplication21/ConsoleApplication21.cpp
@@ -1,18 +1,21 @@
 ﻿#include <stdio.h>
 int main() {
-	int x, y;
-	long long result = 1;
-	// Ввод числа и степени с клавиатуры
-	printf("Введите число: ");
-	scanf("%d", &x);
-	printf("Введите степень: ");
-	scanf("%d", &y);
-	// Вычисление результата
-	for (int i = 0; i < y; i++)
-	{
-		result *= x;
-	}
-	//Вывод результата
-	printf("%d в степени %d равно %d\n", x, y, result);
-	return 0;
+    int x, y;
+    long long result = 1;
+    // Ввод числа и степени с клавиатуры
+    printf("Введите число: ");
+    scanf("%d", &x);
+    printf("Введите степень: ");
+    scanf("%d", &y);
+    // Вычисление результата
+    for (int i = 0; i < y; i++)
+    {
+        result *= x;
+    }
+    //Вывод результата
+    printf("%d в степени %d равно %d\n", x, y, result);
+    result *= 2;
+    printf("Значение результат, умноженное на 2: %d\n", result);
+
+    return 0;
 }
