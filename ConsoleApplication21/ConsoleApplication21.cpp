747c5f07cdff82e7fe39d43aef5c4fed08f130b6
bvzgjkz <122781449+bvzgjkz@users.noreply.github.com>
2024-02-27 05:58:09 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication21/ConsoleApplication21.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main() {
+	int x, y;
+	long long result = 1;
+	// Ввод числа и степени с клавиатуры
+	printf("Введите число: ");
+	scanf("%d", &x);
+	printf("Введите степень: ");
+	scanf("%d", &y);
+	// Вычисление результата
+	for (int i = 0; i < y; i++)
+	{
+		result *= x;
+	}
+	//Вывод результата
+	printf("%d в степени %d равно %d\n", x, y, result);
+	return 0;
+}
